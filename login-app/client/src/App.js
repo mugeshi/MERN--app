f2e91566e67e92e289c90c0b4f5112f159ab5703
mugeshi <nicolemugeshi@gmail.com>
2024-02-26 13:41:47 +0300

    updated index.js

--- login-app/client/src/App.js
+++ login-app/client/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     <h1>React App</h1> 
-    </div>
-  );
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
