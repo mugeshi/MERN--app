77347257451f84b4fbdae6d3de914ba5e6577a59
mugeshi <nicolemugeshi@gmail.com>
2024-03-01 00:59:09 +0300

    updated login validation

--- login-app/client/src/components/Password.js
+++ login-app/client/src/components/Password.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import avatar from '../assets/profile.png';
+import styles from '../styles/Username.module.css';
+import {Toaster} from 'react-hot-toast';
+import {useFormik}  from 'formik'
+import {usernameValidate} from '../helper/validate'
 
 export default function Password() {
+
+
+  const formik =useFormik({
+    initialValues :{
+      Password : ''
+    },
+    validate:  usernameValidate, 
+    validateOnBlur: false,
+    validationOnChange: false,
+    onSubmit : async values => {
+      console.log(values)
+    }
+  })
+
   return (
-    <div>Password</div>
-  )
+    <div className="container mx-auto">
+
+     
+     <Toaster className="top-centre" reverseOrder="false" ></Toaster>
+
+
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello Again</h4>
+            <span className="py-4 text-xl w-2/3 text-center black">
+              Explore More by connecting with
+            </span>
+          </div>
+
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className='profile flex justify-center py-4'>
+              <img src={avatar} className={styles.profile_img} alt="avatar"/>
+            </div>
+
+            <div className="textbox flex flex-col items-center">
+              <input {...formik.getFieldProps('Password ')}  className={styles.textbox} type="text" placeholder='Username'/>
+              <button className={styles.btn} type='submit'>Let's Go</button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className='text-gray-500'>Not a Member<Link className='text-red-500' to="/register">Register Now</Link></span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- login-app/client/src/helper/validate.js
+++ login-app/client/src/helper/validate.js
@@ -9,6 +9,36 @@ export async function usernameValidate(values){
 }
 
 
+//validate password
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+
+
+//Validate password
+function passwordVerify(errors = {}, values){
+     
+    const specialchars = "! @ # $ % ^ & * ( ) _ + - = { } [ ] | ; : ' , . < > / ?"
+
+    if(!values.password){
+        errors.password = toast.error("password Required...!");
+    }else if (values.password.include(" ")){
+        errors.password = toast.error("wrong password...!");
+    }else if (values.password.length < 4){
+         errors.password = toast.error("Password must be more than 4 character long");
+    }else if(!specialchars.test(values.password)){
+          errors.password = toast.error("Password must have speacial characters");
+    }
+
+    return error;
+}
+
+
+
+
 
 //validate username
 function usernameVerify(error = {}, values){
@@ -21,3 +51,5 @@ function usernameVerify(error = {}, values){
 
     return error;
 }
+
+
